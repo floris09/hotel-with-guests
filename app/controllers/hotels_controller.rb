69be0b6d47eea771bfeb0a26c7eef6f5a7b087c4
floris09 <florismeininger@gmail.com>
2017-10-08 18:38:46 +0200

    Add full site without styling

---
+++ app/controllers/hotels_controller.rb
@@ -0,0 +1,11 @@
+class HotelsController < ApplicationController
+
+  def index
+    @hotels = Hotel.all
+  end
+
+  def show
+    @hotel = Hotel.find(params[:id])
+  end
+
+end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,7 @@
 class Hotel < ApplicationRecord
+  has_many :guests
+
+  def self.order_by_name
+    order(:name)
+  end
 end
